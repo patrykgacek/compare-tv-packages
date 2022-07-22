874fd59c81cc98c9d07ec1bcd1d48bec753d07b4
Gacus <36047400+Gacus@users.noreply.github.com>
2022-07-22 19:09:28 +0200

    Update data UPC

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,7 @@
 import { artcom } from "./data/artcom"
 import { netia } from "./data/netia"
 import { canalplusTmp } from "./data/canalplusTmp"
+import { upc } from "./data/upc"
 
 export const getPackages = (provider) => {
   const providerName = provider.name
@@ -16,7 +17,8 @@ export const preparePrograms = () => {
   const netiaAll = getPackages(netia)
   const artcomAll = getPackages(artcom)
   const canalplusTmpAll = getPackages(canalplusTmp)
-  return netiaAll.concat(artcomAll).concat(canalplusTmpAll)
+  const upcAll = getPackages(upc)
+  return netiaAll.concat(artcomAll).concat(canalplusTmpAll).concat(upcAll)
 }
 
 export const packages = preparePrograms()
